81d3d70ff11ebf407f1f1f86e22ea8845e41903e
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-16 10:55:32 -0300

    create-view-shortcode3

--- wp-content/plugins/código inicial.php
+++ wp-content/plugins/código inicial.php
@@ -1,4 +1,4 @@
-<h3><?php echo ( ! empty ( $content ) ) ? esc_html( $content ) : esc_html( MV_Slider_Settings::$options['mv_slider_title'] ); ?></h3>
+<h3>Title</h3>
 <div class="mv-slider flexslider ">
     <ul class="slides">
         <li>
