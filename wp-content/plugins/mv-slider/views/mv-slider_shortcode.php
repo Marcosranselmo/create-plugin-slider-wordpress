4c49f0a3653a650eca52f1bf0ca911c7d1efbde2
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-17 11:15:04 -0300

    slider-custom

--- wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -1,5 +1,5 @@
 <h3><?php echo ( ! empty ( $content ) ) ? esc_html( $content ) : esc_html( MV_Slider_Settings::$options['mv_slider_title'] ); ?></h3>
-<div class="mv-slider flexslider ">
+<div class="mv-slider flexslider <?php echo ( isset( MV_Slider_Settings::$options['mv_slider_style'] ) ) ? esc_attr( MV_Slider_Settings::$options['mv_slider_style'] ) : 'style-1'; ?>">
     <ul class="slides">
     <?php 
     
@@ -20,7 +20,14 @@
 
     ?>
         <li>
-        <?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
+        <?php 
+        if( has_post_thumbnail() ){
+            the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) );
+        }else{
+            echo "<img src='" . MV_SLIDER_URL . "assets/images/default.jpg' class='img-fluid wp-post-image' />";
+        }
+         
+        ?>
             <div class="mvs-container">
                 <div class="slider-details-container">
                     <div class="wrapper">
