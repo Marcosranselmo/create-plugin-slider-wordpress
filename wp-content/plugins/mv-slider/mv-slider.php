a36a2b0934337e71470010f2e81fd0c219c8c8da
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-17 09:37:05 -0300

    shortcode-registering-and-queuing

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -46,6 +46,8 @@ if( ! class_exists( 'MV_Slider' ) ){
 
             require_once( MV_SLIDER_PATH . 'shortcodes/class.mv-slider-shortcode.php' );
             $MV_Slider_Shortcode = new MV_Slider_Shortcode();
+
+            add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ), 999 );
         }
 
         public function define_constants(){
@@ -111,6 +113,13 @@ if( ! class_exists( 'MV_Slider' ) ){
             settings_errors( 'mv_slider_options' );
 
             require( MV_SLIDER_PATH . 'views/settings-page.php' );
+        }
+
+        public function register_scripts(){
+            wp_register_script( 'mv-slider-main-jq', MV_SLIDER_URL . 'vendor/flexslider/jquery.flexslider-min.js', array( 'jquery' ), MV_SLIDER_VERSION, true );
+            wp_register_script( 'mv-slider-options-js', MV_SLIDER_URL . 'vendor/flexslider/flexslider.js', array( 'jquery' ), MV_SLIDER_VERSION, true );
+            wp_register_style( 'mv-slider-main-css', MV_SLIDER_URL . 'vendor/flexslider/flexslider.css', array(), MV_SLIDER_VERSION, 'all' );
+            wp_register_style( 'mv-slider-style-css', MV_SLIDER_URL . 'assets/css/frontend.css', array(), MV_SLIDER_VERSION, 'all' );
         }
 
     }
