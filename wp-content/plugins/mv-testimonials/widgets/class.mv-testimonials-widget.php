799ceaf28ae060d52a508cd32d792a17582f209a
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-26 16:28:56 -0300

    API-Widget-front-end2

--- wp-content/plugins/mv-testimonials/widgets/class.mv-testimonials-widget.php
+++ wp-content/plugins/mv-testimonials/widgets/class.mv-testimonials-widget.php
@@ -29,32 +29,27 @@ class MV_Testimonials_Widget extends WP_Widget{
         $company = isset( $instance['company'] ) ? (bool) $instance['company'] : false;
         ?>
             <p>
-                <label for="<?php echo $this->get_field_id( 'title' ); ?><?php esc_html_e( 'Title', 'mv-testimonials' ); ?>:"></label>
-                <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo 
-                $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>">
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title', 'mv-testimonials' ); ?>:</label>
+                <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>">
             </p>
 
             <p>
-                <label for="<?php echo $this->get_field_id( 'number' ); ?><?php esc_html_e( 'Number of testimonials to show', 'mv-testimonials' ); ?>:"></label>
-                <input type="number" class="tiny-test" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo 
-                $this->get_field_name( 'number' ); ?>" step="1" min="1" size="3" value="<?php echo esc_attr( $number ); ?>">
+                <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number of testimonials to show', 'mv-testimonials' ); ?>:</label>
+                <input type="number" class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" step="1" min="1" size="3" value="<?php echo esc_attr( $number ); ?>">
             </p>
 
             <p>
-                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo 
-                $this->get_field_name( 'image' ); ?>" <?php checked( $image ); ?>>
-                <label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php esc_html_e( 'display user iamge?', 'mv-testimonials' ); ?></label>
+                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" <?php checked( $image ); ?>>
+                <label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php esc_html_e( 'Display user image?', 'mv-testimonials' ); ?></label>
             </p>
 
             <p>
-                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'occupation' ); ?>" name="<?php echo 
-                $this->get_field_name( 'occupation' ); ?>" <?php checked( $occupation ); ?>>
+                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'occupation' ); ?>" name="<?php echo $this->get_field_name( 'occupation' ); ?>" <?php checked( $occupation ); ?>>
                 <label for="<?php echo $this->get_field_id( 'occupation' ); ?>"><?php esc_html_e( 'Display occupation?', 'mv-testimonials' ); ?></label>
             </p>
 
             <p>
-                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'company' ); ?>" name="<?php echo 
-                $this->get_field_name( 'company' ); ?>" <?php checked( $company ); ?>>
+                <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'company' ); ?>" name="<?php echo $this->get_field_name( 'company' ); ?>" <?php checked( $company ); ?>>
                 <label for="<?php echo $this->get_field_id( 'company' ); ?>"><?php esc_html_e( 'Display company?', 'mv-testimonials' ); ?></label>
             </p>
 
@@ -67,10 +62,13 @@ class MV_Testimonials_Widget extends WP_Widget{
         $number = ! empty( $instance['number'] ) ? $instance['number'] : 5;
         $image = isset( $instance['image'] ) ? $instance['image'] : false;
         $occupation = isset( $instance['occupation'] ) ? $instance['occupation'] : false;
-        $compay = isset( $instance['company'] ) ? $instance['company'] : false;
+        $company = isset( $instance['company'] ) ? $instance['company'] : false;
 
         echo $args['before_widget'];
         echo $args['before_title'] . $title . $args['after_title'];
+
+        require( MV_TESTIMONIALS_PATH . 'views/mv-testimonials_widget.php' );
+        
         echo $args['after_widget'];
     }
 
@@ -78,7 +76,7 @@ class MV_Testimonials_Widget extends WP_Widget{
         $instance = $old_instance;
         $instance['title'] = sanitize_text_field( $new_instance['title'] );
         $instance['number'] = (int) $new_instance['number'];
-        $instance['image'] = ! empty ( $new_instance['title'] ) ? 1 : 0;
+        $instance['image'] = ! empty ( $new_instance['image'] ) ? 1 : 0;
         $instance['occupation'] = ! empty ( $new_instance['occupation'] ) ? 1 : 0;
         $instance['company'] = ! empty ( $new_instance['company'] ) ? 1 : 0;
         return $instance;
