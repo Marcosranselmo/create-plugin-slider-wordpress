0c72623437e6b8e4d3ae55539cb15913ca06e592
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-29 10:30:36 -0300

    create-template-for-plugin

--- wp-content/plugins/mv-testimonials/views/mv-testimonials_widget.php
+++ wp-content/plugins/mv-testimonials/views/mv-testimonials_widget.php
@@ -45,3 +45,5 @@
     wp_reset_postdata(); 
 endif;
 ?>
+<a href="<?php echo get_post_type_archive_link( 'mv-testimonials' ); ?>"><?php echo esc_html_e( 'Show More Testimonials',
+'mv-testimonials' ); ?></a>
