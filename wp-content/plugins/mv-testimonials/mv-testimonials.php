b7402f7ae4b25b5fa5492f829cac388f33535e20
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-30 09:10:40 -0300

    delete-plugin

--- wp-content/plugins/mv-testimonials/mv-testimonials.php
+++ wp-content/plugins/mv-testimonials/mv-testimonials.php
@@ -120,6 +120,19 @@ if( !class_exists( 'MV_Testimonials' ) ){
          */
         public static function uninstall(){
 
+            delete_option( 'widget_mv-testimonials' );
+
+            $posts = get_posts(
+                array(
+                    'post_type' => 'mv-testimonials',
+                    'number_posts'  => -1,
+                    'post_status'   => 'any'
+                )
+            );
+
+            foreach( $posts as $post ){
+                wp_delete_post( $post->ID, true );
+            }
         }
 
     }
