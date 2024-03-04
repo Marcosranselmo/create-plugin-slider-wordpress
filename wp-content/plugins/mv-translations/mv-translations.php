38675a4b1ce28db198d232972d387db6783c764d
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-03-03 21:01:48 -0300

    criando-classe-do-primeiro-shortcode

--- wp-content/plugins/mv-translations/mv-translations.php
+++ wp-content/plugins/mv-translations/mv-translations.php
@@ -43,6 +43,9 @@ if( !class_exists( 'MV_Translations' )){
 
             require_once( MV_TRANSLATIONS_PATH . "post-types/class.mv-translations-cpt.php" );
             $MVTranslationsPostType = new MV_Translations_Post_Type();
+
+            require_once( MV_TRANSLATIONS_PATH . "shortcodes/class.mv-translations-shortcode.php" );
+            $MVTranslationsShortcode = new MV_Translations_Shortcode();
             			
 		}
 
